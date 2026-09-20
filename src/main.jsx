25773f24d3e82d661b11bd64b150e9cc17abd403
crazy6-des <1234ymarmu@gmail.com>
2026-09-20 00:54:34 -0400

    Load S design system after existing styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,7 @@
-import React from "react";import{createRoot}from"react-dom/client";import App from "./App";import"./styles.css";createRoot(document.getElementById("root")).render(<App/>);
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./styles.css";
+import "./ui/designSystem.css";
+
+createRoot(document.getElementById("root")).render(<App />);
