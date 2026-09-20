f9e503cd6f8fead02e65da5c2359ade9035fb9a2
crazy6-des <1234ymarmu@gmail.com>
2026-09-20 03:20:18 -0400

    Use window Event for router navigation

--- src/app/routes.js
+++ src/app/routes.js
@@ -29,7 +29,7 @@ export function navigateTo(route) {
   const next = normalizeRoute(route);
   if (window.location.pathname !== next) {
     window.history.pushState({}, "", next);
-    window.dispatchEvent(new Event("popstate"));
+    window.dispatchEvent(new window.Event("popstate"));
   }
   return next;
 }
