4f689d7f58f2feab1dd15fb806af2d23d3303faa
crazy6-des <1234ymarmu@gmail.com>
2026-09-20 03:52:04 -0400

    Define canonical S product identity

---
+++ src/app/productIdentity.js
@@ -0,0 +1,9 @@
+export const PRODUCT_IDENTITY = Object.freeze({
+  name: "S",
+  tagline: "Where you are truly seen. Where you connect.",
+  composerPrompt: "What do you want to share?",
+  composerHint: "Share a thought, image, sound or moment.",
+  searchPlaceholder: "Search S",
+  activityTitle: "On S now",
+  postedMessage: "Posted to S",
+});
