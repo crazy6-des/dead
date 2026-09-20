9dc5440f612e6581aa617152645fa7c2b4d41e8f
crazy6-des <1234ymarmu@gmail.com>
2026-09-20 01:05:38 -0400

    Export shared interaction-state helpers

--- src/features/index.js
+++ src/features/index.js
@@ -1,2 +1,9 @@
 export { FEED_TABS, selectFeed, searchPosts } from "./feed/feedSelectors";
 export { toggleLike, toggleSaved, followPostAuthor } from "./social/socialState";
+export {
+  INITIAL_INTERACTION_STATE,
+  createInteractionState,
+  isInteractionBusy,
+  hasInteractionError,
+  getInteractionMessage,
+} from "../ui/interactionState";
