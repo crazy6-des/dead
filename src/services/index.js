167b9eb8614ff1fac4fcd2279654b7b4b17778cf
crazy6-des <1234ymarmu@gmail.com>
2026-09-20 03:37:54 -0400

    Export social communication services

--- src/services/index.js
+++ src/services/index.js
@@ -1,15 +1,6 @@
 export { ApiError, apiClient, apiRequest, hasApiBaseUrl } from "./apiClient.js";
 export { authService } from "./authService.js";
-export {
-  createFeedRequest,
-  createFeedResponse,
-  createDevFeedAdapter,
-  createApiFeedAdapter,
-  createFeedAdapter,
-} from "./feedService.js";
-export {
-  createPostRequest,
-  createApiPostAdapter,
-  createDevPostAdapter,
-  createPostAdapter,
-} from "./postService.js";
+export { createFeedRequest, createFeedResponse, createDevFeedAdapter, createApiFeedAdapter, createFeedAdapter } from "./feedService.js";
+export { createPostRequest, createApiPostAdapter, createDevPostAdapter, createPostAdapter } from "./postService.js";
+export { notificationService, createNotificationAdapter, createApiNotificationAdapter, createDevNotificationAdapter } from "./notificationService.js";
+export { messageService, createMessageAdapter, createApiMessageAdapter, createDevMessageAdapter } from "./messageService.js";
