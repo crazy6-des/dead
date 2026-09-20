278714fb9090e984a606cd70cdd6eb06cd501fe8
crazy6-des <1234ymarmu@gmail.com>
2026-09-20 01:11:32 -0400

    Export feed view-model helpers

--- src/features/index.js
+++ src/features/index.js
@@ -1,4 +1,5 @@
 export { FEED_TABS, selectFeed, searchPosts } from "./feed/feedSelectors";
+export { FEED_ACTIONS, toFeedPostViewModel, toFeedPostViewModels } from "./feed/feedViewModel";
 export { toggleLike, toggleSaved, followPostAuthor } from "./social/socialState";
 export {
   INITIAL_INTERACTION_STATE,
