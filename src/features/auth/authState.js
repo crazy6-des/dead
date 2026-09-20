940447c144c387b52d8e8d51dfb13067e43fb511
crazy6-des <1234ymarmu@gmail.com>
2026-09-20 02:28:03 -0400

    Fix auth state import alignment

--- src/features/auth/authState.js
+++ src/features/auth/authState.js
@@ -1,5 +1,5 @@
+import { useCallback, useEffect, useMemo, useState } from "react";
 import { authService } from "../../services/authService.js";
-import { authService } from "../../services/authService";
 
 export const AUTH_STATUS = Object.freeze({
   UNKNOWN: "unknown",
