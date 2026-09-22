283b71f70691d4ec4b67e0bdd3a6f494e37a5a71
crazy6-des <1234ymarmu@gmail.com>
2026-09-22 14:39:51 -0400

    Remove obsolete feed preview bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import PreviewMode from "./preview/PreviewMode";
 import "./styles.css";
 import "./ui/designSystem.css";
 import "./ui/alignment.css";
 import "./ui/mobileNavigation.css";
 import "./ui/visualPolish.css";
 
-const isFeedPreview = new URLSearchParams(window.location.search).get("preview") === "feed";
-
-createRoot(document.getElementById("root")).render(isFeedPreview ? <PreviewMode /> : <App />);
+createRoot(document.getElementById("root")).render(<App />);
